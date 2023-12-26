4e9dfdeea57ad6a7814fb68b1da11b61ab249fbe
sharatsharathshanmukh <sarathponugupati@gmail.com>
2023-12-25 19:29:22 -0500

    added links

--- Sanju_portfolio/src/Components/NavBar.tsx
+++ Sanju_portfolio/src/Components/NavBar.tsx
@@ -35,7 +35,7 @@ const NavBar = () => {
         </div>
         <ul className='hidden md:flex '>
             {links.map(({id,link})=><li className='px-3  cursor-pointer capitalize font-extralight text-gray-500 hover:scale-150 duration-150' key={id}>
-                <Link  to={link} smooth duration={500}>{link}</Link>
+                <Link to={link} smooth duration={500}>{link}</Link>
             
             
             </li>
@@ -52,7 +52,7 @@ const NavBar = () => {
         
             {nav &&             <ul className='flex flex-col justify-center items-center absolute
             top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
-          {links.map(({id,link})=><li className='px-4 py-6  cursor-pointer capitalize font-extralight text-gray-500 hover:scale-150 duration-150 text-4xl' key={id}>{link}</li>
+          {links.map(({id,link})=><li className='px-4 py-6  cursor-pointer capitalize font-extralight text-gray-500 hover:scale-150 duration-150 text-4xl' key={id}><Link onClick={()=>setNav(!nav)} to={link} smooth duration={500}>{link}</Link></li>
 
 
 )}
